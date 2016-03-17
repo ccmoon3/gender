185566a3f439ab0739643d976e4a72ef73115dd1
XY Chen <mail-999@qq.com>
2016-03-17 17:20:14 -0400

    false

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,83 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.config(function($compileProvider){
+$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile|content):|data:image\//);
+})
+
+.controller('CameraCtrl', function($scope, $cordovaCamera) {
+  /* $scope.takePhoto = function() {
+         Camera.getPicture().then(function(imageURI) {
+              console.log(imageURI);
+              $scope.lastPhoto = imageURI;
+              $scope.ImageURI =  imageURI;
+         //     UploadPicture(imageURI);
+         }, function(err) {
+            console.err(err);
+         }, {
+             quality: 75,
+             allowEdit : true,
+             saveToPhotoAlbum: true
+         });
+    }
+
+  $scope.ShowPictures = function(){
+                navigator.camera.getPicture(  UploadPicture, function(message) {
+                                },{
+                                quality: 75,
+                                destinationType: navigator.camera.DestinationType.FILE_URI,
+                                sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+                            }
+               );
+  }*/
+ /* document.addEventListener("deviceready", onDeviceReady, false);
+
+  function onDeviceReady() {
+
+      console.log("getIn DeviceReady");
+      $scope.takePhoto = function() {
+            var options = {
+              quality: 50,
+              destinationType: Camera.DestinationType.DATA_URL,
+              sourceType: Camera.PictureSourceType.CAMERA,
+              allowEdit: true,
+              encodingType: Camera.EncodingType.JPEG,
+              targetWidth: 100,
+              targetHeight: 100,
+              popoverOptions: CameraPopoverOptions,
+              saveToPhotoAlbum: true,
+              correctOrientation:true
+            };
+            console.log("getIn TakePhoto");
+
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+              var image = document.getElementById('myImage');
+              image.src = "data:image/jpeg;base64," + imageData;
+              console.log("getPicture Now");
+            }, function(err) {
+              // error
+            });
+      }
+  }*/
+
+                  $scope.takePhoto = function () {
+                    var options = {
+                      quality: 75,
+                      allowEdit: true,
+                      encodingType: Camera.EncodingType.JPEG,
+                      saveToPhotoAlbum: true
+                  };
+                   console.log("getIn TakePhoto");
+
+                      $cordovaCamera.getPicture(options).then(function (imageData) {
+                          console.log("getPicture Now");
+                          $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                      }, function (err) {
+                          console.log("getPicture Error");
+                          // An error occured. Show a message to the user
+                      });
+                  }
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
